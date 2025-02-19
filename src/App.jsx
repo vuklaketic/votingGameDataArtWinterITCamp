e67a4818cb112c069033be711d91383abc3c796c
Vuk Laketic <vuklaketic@gmail.com>
2025-02-19 20:18:40 +0100

    Added index.css and App.jsx files in src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Voting Game</h1>
+      <JokeList />
+    </div>
+  );
+};
+
+export default App;
